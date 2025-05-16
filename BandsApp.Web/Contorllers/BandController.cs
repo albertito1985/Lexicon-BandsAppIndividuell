e68324d512fbabda712708fb761af90d392787aa
albertito1985 <anulo@live.se>
2025-05-16 09:27:17 +0200

    Add project files.

---
+++ BandsApp.Web/Contorllers/BandController.cs
@@ -0,0 +1,24 @@
+﻿using BandsApp.Web.Models;
+using BandsApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandsApp.Web.Contorllers
+{
+    public class BandController: Controller
+    {
+        static BandService service = new BandService();
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(service.GetAll());
+        }
+
+        [HttpPost("/Add")]
+        public IActionResult AddBand(Band newBand)
+        {
+            service.AddBand(newBand);
+            return View(nameof(Index));
+        }
+    }
+}
---
+++ BandsApp.Web/Services/BandService.cs
@@ -0,0 +1,30 @@
+﻿using BandsApp.Web.Models;
+
+namespace BandsApp.Web.Services
+{
+    public class BandService
+    {
+        private List<Band> bands = [
+            new Band {
+                Id=123,
+                Name = "Radiohead"
+            },
+            new Band {
+                Id=462,
+                Name = "The Smiths"
+            },
+            new Band {
+                Id=55,
+                Name = "Los Pericos"
+            }
+           ];
+
+        public Band[] GetAll() => bands.ToArray();
+
+        public void AddBand(Band band)
+        {
+            band.Id = bands.Count == 0 ? 1 : bands.Max(b => b.Id)+1;
+            bands.Add(band);
+        }
+    }
+}
